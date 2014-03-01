bb4b8cd59ace0bbd1443e7e56dd7a20e89ad0efb
小傅Fox <xfox@cotr.me>
2014-03-01 20:16:16 +0800

    2014年3月1日[2] 1.修正错误

--- hook.php
+++ hook.php
@@ -5,47 +5,46 @@ include_once './api.php';
 try{
     $r = ob_get_contents();
     $json = json_decode( $_POST[ 'payload' ] , true );
-    $f_config = fopen( @'./config.json' , 'r' );
-    $config = json_decode( fread( $f_config , filesize( @'./config.json' ) ) , true );
+    $f_config = fopen( 'config.json' , 'r' );
+    $config_text = fread( $f_config , filesize( 'config.json' ) );
+    $config = json_decode( $config_text , true );
     fclose( $f_config );
-    $config = $config[ 'repos' ];
     if( $_SERVER[ 'HTTP_X_GITHUB_EVENT' ] == 'push' ){
         $commits = $json[ 'commits' ];
         $repo = $json[ 'repository' ][ 'name' ];
-        $weibo = $config[ $repo ][ 'commit' ];
+        $weibo = $config[ 'repos' ][ $repo ][ 'commit' ];
         foreach( $commits as $value ){
-            $c_uid = $config[ 'users' ][$value[ 'committer' ][ 'username' ]];
-            $a_uid = $config[ 'users' ][$value[ 'author' ][ 'username' ]];
-            $c_arr=WeiboPHP_cURL_Get( 'https://api.weibo.com/2/users/show.json' , array( 
-                    'source' => 3100975615 , 
-                    'uid' => $c_uid
+            $c_uid = $config[ 'users' ][ $value[ 'committer' ][ 'username' ] ];
+            $a_uid = $config[ 'users' ][ $value[ 'author' ][ 'username' ] ];
+            $value2 = $config[ 'senders' ][ 0 ];
+            $sina = new WeiboPHP( $value2[ 'name' ] , $value2[ 'password' ] , $value2[ 'key' ] , $value2[ 'secret' ] , $value2[ 'uri' ] );
+            $c_arr = $sina -> HTTPGet( 'users/show.json' , array( 
+                    'source' => 211160679 , 
+                    'uid' => $c_uid 
             ) );
-            $c_uname=$c_arr['screen_name'];
-            $a_arr=WeiboPHP_cURL_Get( 'https://api.weibo.com/2/users/show.json' , array(
-                    'source' => 3100975615 ,
-                    'uid' => $a_uid
+            $c_uname = '@' . $c_arr[ 'name' ];
+            $a_arr = $sina -> HTTPGet( 'users/show.json' , array( 
+                    'source' => 211160679 , 
+                    'uid' => $a_uid 
             ) );
-            $a_uname=$a_arr['screen_name'];
+            $a_uname = '@' . $a_arr[ 'name' ];
             $params = array( 
                     '%branch' => $json[ 'ref' ] , 
                     '%url' => $value[ 'url' ] , 
                     '%message_1' => preg_replace( '/\n.*/' , '' , $value[ 'message' ] ) , 
                     '%message' => str_replace( '\n' , ' , ' , $value[ 'message' ] ) , 
-                    '%committer' => $config[ 'users' ][ $c_uname ] , 
-                    '%sha1' => substr( $value[ 'id' ] , 0 , $config[ $repo ][ 'sha1' ] ) , 
-                    '%author' => $config[ 'users' ][ $a_uname ] 
+                    '%committer' => $c_uname , 
+                    '%sha1' => substr( $value[ 'id' ] , 0 , $config[ 'repos' ][ $repo ][ 'sha1' ] ) , 
+                    '%author' => $a_uname 
             );
             foreach( $params as $key => $value1 ){
                 $weibo = str_replace( $key , $value1 , $weibo );
             }
-            /*
-             * //xiaofu fox $sina = new WeiboPHP( 'fox.q@foxmail.com' , 'CoderFox19990903' , '3100975615' , 'b4ecbc14a8ce97bc355345589c8f48a4' , 'http://cloud.cotr.me/' ); var_dump( $sina -> HTTPPost( 'statuses/update.json' , array( 'status' => $weibo ) ) ); //cotr $sina = new WeiboPHP( 'jukewg@sina.com' , 'rtkg^~kN!pnS5e-o' , '3100975615' , 'b4ecbc14a8ce97bc355345589c8f48a4' , 'http://cloud.cotr.me/' ); var_dump( $sina -> HTTPPost( 'statuses/update.json' , array( 'status' => $weibo ) ) );
-             */
-            foreach($config['senders'] as $value2){
-                $sina = new WeiboPHP( $value2['name'] , $value2['password'] ,
-                         $value2['key'] , $value2['secret'] ,
-                         $value2['uri'] );
-                var_dump( $sina -> HTTPPost( 'statuses/update.json' , array( 'status' => $weibo ) ) );
+            foreach( $config[ 'senders' ] as $value2 ){
+                $sina = new WeiboPHP( $value2[ 'name' ] , $value2[ 'password' ] , $value2[ 'key' ] , $value2[ 'secret' ] , $value2[ 'uri' ] );
+                var_dump( $sina -> HTTPPost( 'statuses/update.json' , array( 
+                        'status' => $weibo 
+                ) ) );
             }
         }
     }
